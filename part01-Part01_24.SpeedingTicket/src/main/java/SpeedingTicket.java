c8bb88c67c045989708fed1cf42473e269ab2e84
bogdanminne <bogdannsdap@gmail.com>
2025-04-28 07:55:03 +0300

    Adding exercises

--- part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
+++ part01-Part01_24.SpeedingTicket/src/main/java/SpeedingTicket.java
@@ -7,5 +7,11 @@ public class SpeedingTicket {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here. 
+        System.out.println("Give speed: ");
+        int speed = scanner.nextInt();
+        if (speed > 120) {
+            System.out.println("Speeding ticket!");
+        }
+        
     }
 }
--- part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -6,5 +6,18 @@ public class LargerThanOrEqualTo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("Give the first number: ");
+        int firstNumber = scan.nextInt();
+        System.out.print("Give the second number: ");
+        int secondNumber = scan.nextInt();
+
+        if (firstNumber > secondNumber) {
+            System.out.println("Greater number is: " + firstNumber);
+        } else if (firstNumber == secondNumber) {
+            System.out.println("The numbers are equal!");
+        } else {
+            System.out.println("Greater number is: " + secondNumber);
+        }
+
     }
 }
