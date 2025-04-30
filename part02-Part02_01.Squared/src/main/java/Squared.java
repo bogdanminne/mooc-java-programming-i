8ab150c02f2cf24471648055ff24a3b77f34f73c
bogdanminne <bogdannsdap@gmail.com>
2025-04-30 09:26:09 +0300

    Adding exercises 02_01 - 02_04

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -6,5 +6,10 @@ public class Squared {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = scanner.nextInt();
+        int squared = number * number;
+
+        System.out.println(squared);
+
     }
 }
--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -6,5 +6,12 @@ public class SquareRootOfSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+
+        double square = Math.sqrt(number1 + number2);
+
+        System.out.println(square);
+
     }
 }
