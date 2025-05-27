1ff1fcfd4abe17b68d4f8263c10cab5581f651e2
bogdanminne <bogdannsdap@gmail.com>
2025-05-27 16:22:31 +0300

    Adding exercises 03_01 - 03_03

--- part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,28 +1,19 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sandbox {
-
     public static void main(String[] args) {
-        // Write your program here
-        Scanner scanner = new Scanner(System.in);
-        int values = 0;
-        int sum = 0;
 
-        while (true) {
-            System.out.println("Provide a value, a negative value ends the program");
-            int value = scanner.nextInt();
-            if (value < 0) {
-                break;
-            }
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Input a string: ");
+        String a = input.nextLine();
 
-            values = values + 1;
-            sum = sum + value;
-        }
+        String[] splitted = a.split("(?!^)");
 
-        if (sum == 0) {
-            System.out.println("The average of the values could not be calculated.");
-        } else {
-            System.out.println("Average of values: " + (1.0 * sum / values));
-        }
-            }
+        System.out.println(Arrays.toString(splitted));
+
+        
+
+    }
 }
