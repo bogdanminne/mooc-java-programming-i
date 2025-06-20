87bd3bf6469842dfee01c79a104a043e546ebc07
bogdanminne <bogdannsdap@gmail.com>
2025-06-20 08:11:48 +0300

    Adding exercises 03_22 - 03_25

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -1,10 +1,39 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+
+        ArrayList<String> array = new ArrayList<String>();
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("")) {break;}
+
+            array.add(input);
+
+        }
+
+
+
+
+
+
+
+
+
+    
+
+       
+
+        for (int i = 0; i < array.size(); i++) {
+            System.out.println(array.get(i));
+        }
 
 
     }
